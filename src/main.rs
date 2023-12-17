c2c1e5467abd19e9d1affea91d0b3ff7f0b153a0
Alfonso Alba García <alfonso@alfonsoalba.com>
2023-12-17 09:53:10 +0100

    Read branches from repository and show them in a Select box

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use cursive::event::EventResult;
 use cursive::traits::*;
 use cursive::views::{Dialog, OnEventView, SelectView, TextView};
 use cursive::Cursive;
+use git2::{BranchType, Repository};
 
 // We'll use a SelectView here.
 //
@@ -10,18 +11,48 @@ use cursive::Cursive;
 // one.
 
 fn main() {
+    let repo = match Repository::open("./") {
+        Ok(repo) => repo,
+        Err(e) => panic!("failed to open: {}", e),
+    };
+    let local_branch_option: Option<BranchType> = Some(BranchType::Local);
+
+    let local_branches = match repo.branches(local_branch_option) {
+        Ok(branches) => branches,
+        Err(e) => panic!("failed to open: {}", e),
+    };
+    for result in local_branches {
+        match result {
+            Ok((b, _btype)) => match b.name() {
+                Ok(n) => println!("{}", n.unwrap()),
+                Err(e) => panic!("error! {}", e),
+            },
+            Err(e) => panic!("error! {}", e),
+        }
+    }
+
+    let local_branches = match repo.branches(local_branch_option) {
+        Ok(branches) => branches,
+        Err(e) => panic!("failed to get branches: {}", e),
+    };
+    let mut branch_names: Vec<String> = Vec::new();
+    for result in local_branches {
+        match result {
+            Ok((b, _btype)) => match b.name() {
+                Ok(n) => branch_names.push(String::from(n.unwrap())),
+                Err(e) => panic!("error! {}", e),
+            },
+            Err(e) => panic!("error! {}", e),
+        };
+    }
+
     let mut select: SelectView = SelectView::new()
         // Center the text horizontally
         .h_align(HAlign::Center)
         // Use keyboard to jump to the pressed letters
         .autojump();
 
-    // Read the list of cities from separate file, and fill the view with it.
-    // (We include the file at compile-time to avoid runtime read errors.)
-    //let content = include_str!("assets/cities.txt");
-    //select.add_all_str(content.lines());
-    let content = ["Line 1", "Line 2"];
-    select.add_all_str(content);
+    select.add_all_str(branch_names);
 
     // Sets the callback for when "Enter" is pressed.
     select.set_on_submit(show_next_window);
@@ -42,7 +73,7 @@ fn main() {
     // Let's add a ResizedView to keep the list at a reasonable size
     // (it can scroll anyway).
     siv.add_layer(
-        Dialog::around(select.scrollable().fixed_size((20, 10))).title("Where are you from?"),
+        Dialog::around(select.scrollable().fixed_size((20, 10))).title("Select your branch..."),
     );
 
     siv.run();
@@ -52,6 +83,6 @@ fn main() {
 // but it's not required.
 fn show_next_window(siv: &mut Cursive, city: &str) {
     siv.pop_layer();
-    let text = format!("{city} is a great city!");
+    let text = format!("let's checkout out {city}");
     siv.add_layer(Dialog::around(TextView::new(text)).button("Quit", |s| s.quit()));
 }
