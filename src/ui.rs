9026383b11900561522556e9ca7e3bafd2f49f8e
Alfonso Alba García <alfonso@alfonsoalba.com>
2024-01-13 19:47:24 +0100

    chore: extract method to create ui components.

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,5 @@
 use cursive::align::HAlign;
-use cursive::event::EventResult;
-use cursive::views::{Dialog, OnEventView, SelectView, TextView};
+use cursive::views::{Dialog, EditView, LinearLayout, NamedView, SelectView, TextView};
 use cursive::Cursive;
 use cursive::{traits::*, CursiveRunnable};
 
@@ -17,6 +16,22 @@ impl UI {
         }
     }
     pub fn start(&mut self) {
+        let select_view = self.select_view();
+        let edit_view = self.edit_view();
+
+        self.siv.add_layer(
+            Dialog::around(
+                LinearLayout::vertical()
+                    .child(edit_view)
+                    .child(select_view.scrollable().fixed_size((20, 10))),
+            )
+            .title("Select your branch..."),
+        );
+
+        self.siv.run();
+    }
+
+    fn select_view(&self) -> NamedView<SelectView> {
         let mut select: SelectView = SelectView::new()
             // Center the text horizontally
             .h_align(HAlign::Center)
@@ -27,25 +42,16 @@ impl UI {
 
         // Sets the callback for when "Enter" is pressed.
         select.set_on_submit(show_next_window);
+        select.with_name("branches")
+    }
 
-        // Let's override the `j` and `k` keys for navigation
-        let select = OnEventView::new(select)
-            .on_pre_event_inner('k', |s, _| {
-                let cb = s.select_up(1);
-                Some(EventResult::Consumed(Some(cb)))
-            })
-            .on_pre_event_inner('j', |s, _| {
-                let cb = s.select_down(1);
-                Some(EventResult::Consumed(Some(cb)))
-            });
-
-        // Let's add a ResizedView to keep the list at a reasonable size
-        // (it can scroll anyway).
-        self.siv.add_layer(
-            Dialog::around(select.scrollable().fixed_size((20, 10))).title("Select your branch..."),
-        );
-
-        self.siv.run();
+    fn edit_view(&self) -> NamedView<EditView> {
+        EditView::new()
+            // update results every time the query changes
+            //.on_edit(on_edit)
+            // submit the focused (first) item of the matches
+            //.on_submit(on_submit)
+            .with_name("query")
     }
 }
 
